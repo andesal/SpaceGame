00aaa30c3fa981a6469c59b9cbc750a6a19e0a2a
Anders Rantala Hunderi <andersrahu@gmail.com>
2018-04-13 14:46:09 +0200

    made the game wait in the lobby. Have not implemented any network stuff yet

--- core/src/no/progark19/spacegame/screens/LobbyScreen.java
+++ core/src/no/progark19/spacegame/screens/LobbyScreen.java
@@ -1,8 +1,13 @@
 package no.progark19.spacegame.screens;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -13,9 +18,11 @@ import no.progark19.spacegame.SpaceGame;
 public class LobbyScreen implements Screen {
 
     private final SpaceGame game;
+    private BitmapFont font;
+    private GlyphLayout layout;
+    private boolean onSupportedDevice;
 
     private Stage stage;
-    private Skin skin;
 
     private ShapeRenderer shapeRenderer;
 
@@ -23,6 +30,9 @@ public class LobbyScreen implements Screen {
         this.game = game;
         this.stage = new Stage(new FitViewport(SpaceGame.WIDTH, SpaceGame.HEIGHT, game.camera));
         this.shapeRenderer = new ShapeRenderer();
+        this.font = new BitmapFont();
+        this.layout = new GlyphLayout();
+        onSupportedDevice = Gdx.app.getType() != Application.ApplicationType.Android;
     }
 
     @Override
@@ -32,12 +42,33 @@ public class LobbyScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+
+        font.setColor(Color.BLACK);
+        font.getData().setScale(4);
+        layout.setText(font, "LOBBY");
+
+        font.draw(game.batch, layout,
+                SpaceGame.WIDTH/2 - layout.width/2, SpaceGame.HEIGHT - layout.height*0.5f
+        );
+
+
+        font.getData().setScale(1);
+        if (!onSupportedDevice){
+            font.setColor(Color.RED);
+            layout.setText(font, "this device is currently not supported");
+
+        } else {
+            font.setColor(Color.GREEN);
+            layout.setText(font, "Looking for crew");
+        }
+        font.draw(game.batch, layout,
+                SpaceGame.WIDTH/2 - layout.width/2, SpaceGame.HEIGHT/2 - layout.height/2
+        );
         game.batch.end();
-        //TODO Begynn lobbyvirksomhet, setter den kun til å hoppe rett inn i PlayScreen_DEMO for nu
-        game.setScreen(new PlayScreen_DEMO(game));
+        //game.setScreen(new PlayScreen_DEMO(game));
     }
 
     @Override
